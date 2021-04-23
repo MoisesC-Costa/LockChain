6b4515a7fa370eb89c3aa0ced15dede498e61f11
MoisesC-Costa <sesiom.br@gmail.com>
2021-04-22 21:47:44 -0300

    Primeira versão completa do modulo lockchain.py Finalizada

--- src/lockchain.py
+++ src/lockchain.py
@@ -1,11 +1,17 @@
-from sys import prefix
 from typing import Union
-from rsa import key, newkeys, PublicKey, PrivateKey
+from rsa import newkeys, key, PublicKey, PrivateKey
 from os import PathLike
 from hashlib import sha256
 import secrets
 
 class CryptMessage:
+    def __rand_prefix() -> bytes:
+        while True:
+            value_prefix = secrets.randbits(32 * 8)
+            
+            if len(data := int_bytes(value_prefix)) == 32:
+                return data
+
     def __init__(self, data : bytes = b"", key : Union[PrivateKey, PublicKey] = None) -> None:
         self.data = data
         self.key = key
@@ -37,63 +43,63 @@ class CryptMessage:
         return self.data
 
     def encrypt(self) -> bool:
-        if type(self.key) != PrivateKey and type(self.key):
+        if (type(self.key) != PrivateKey) and (type(self.key) != PublicKey):
             return False
         
-        length_key = length_int(self.key.n)
-        length_segment = length_key - 72
-        data_segments = []
-        data = b''
+        lenght_key = length_int(self.key.n)
+        lenght_seg = lenght_key - 72
+        data = b""
         
-        for i in range(0, (length_data := len(self.data)), length_segment):
-            if  not (i + length_segment < length_data):
-                data_segments.append(self.data[i:])
-            
-            else:
-                data_segments.append(self.data[i:i + length_segment])
+        for sep in range(0, lenght_data := len(self.data), lenght_seg):
+            if (sep + lenght_seg) < lenght_data:
+                segment = self.data[sep: sep + lenght_seg]
                 
-        for segment in data_segments:
-            prefix = int.to_bytes(secrets.randbits(32*8), 32, 'big')
-            segment = prefix + segment
+            else:
+                segment = self.data[sep:]
+
+            frame = segment
+            verificador = sha256(frame).digest()
+            packet_prefix = CryptMessage.__rand_prefix()
+            packet = packet_prefix + verificador + frame
+
+            value_packet = int.from_bytes(packet, 'big')
+            calc = pow(value_packet, self.key.e, self.key.n)
+            data += int.to_bytes(calc, lenght_key, 'big')
             
-            verificador = sha256(segment).digest()
-            
-            frame = verificador + segment
-            value = bytes_int(frame)
-            calc = pow(value, self.key.e, self.key.n)
-            
-            data += int.to_bytes(calc, length_key, 'big')
-        
         self.data = data
         return True
-
-    def decrypt(self):
+            
+    def decrypt(self) -> bool:
         if type(self.key) != PrivateKey:
             return False
         
-        length_key = length_int(self.key.n)
-        length_segment = length_key - 72
-        data_segments = []
+        lenght_key = length_int(self.key.n)
+        lenght_sep = lenght_key - 72
         data = b''
         
-        for i in range(0, (length_data := len(self.data)), length_key):
-            if not (i + length_key < length_data):
-                data_segments.append(self.data[i:])
-            
-            else:  
-                data_segments.append(self.data[i:i + length_key])
+        for sep in range(0, lenght_data := len(self.data), lenght_key):
+            if (sep + lenght_key) < lenght_data:
+                segment = self.data[sep: sep + lenght_key]
+                
+            else:
+                segment = self.data[sep:]
 
-        for segment in data_segments:
-            value = bytes_int(segment)
-            calc = pow(value, self.key.d, self.key.n)
-            packet = int_bytes(calc)
+            value_segment = int.from_bytes(segment, 'big')
+            calc = pow(value_segment, self.key.d, self.key.n)
+            packet = int.to_bytes(calc, length_int(calc), 'big')
             
-            verificador = packet[:32]
-            frame = packet[-(length_segment + 32):]
-            data += frame[-length_segment:]
-        
+            checksum = packet[32:64]
+            message = packet[64:]
+            
+            if checksum != sha256(message).digest():
+                print("Falhou o check sum")
+            
+            data += message
+
+            
         self.data = data
         return True
+            
             
 class CryptFile(CryptMessage):
     def __init__(self, file: PathLike = "", key: Union[PrivateKey, PublicKey] = None) -> None:        
@@ -151,11 +157,11 @@ class CryptFile(CryptMessage):
 def length_int(num : int = 0) -> int:
     bits = int.bit_length(num)
     
-    if not bits % 8:
+    if bits % 8 == 0:
         return bits // 8
     
     else:
-        return (bits // 8) + 1
+        return ((bits // 8) + 1)
 
 int_bytes = lambda x : int.to_bytes(x, length_int(x), 'big')
 bytes_int = lambda x: int.from_bytes(x, 'big')
